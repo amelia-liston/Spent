3c9564223b6255da5a44b2cd6b15389a98dc6699
amelia-liston <aliston@bu.edu>
2025-10-04 12:39:54 -0400

    Implemented OAuth with Google

--- project/src/Home.jsx
+++ project/src/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the simple React app! This is the home page.</p>
-    </div>
+    <GoogleOAuthProvider clientId="571696130341-sgejgrap1u1odfgvg53vme8p7f17drp8.apps.googleusercontent.com">
+      <div>
+        <h1>Home Page</h1>
+        <p>Welcome to the simple React app! This is the home page.</p>
+        <div style={{ marginTop: '2rem' }}>
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log('Login Success:', credentialResponse);
+              // You can handle the login response here
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+        </div>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
