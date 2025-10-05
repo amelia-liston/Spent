c63929839895e397134158fd41fb94b6009058c6
amelia-liston <aliston@bu.edu>
2025-10-04 22:12:52 -0400

    Added content to the about page

--- project/src/About.jsx
+++ project/src/About.jsx
@@ -3,8 +3,11 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>This is a simple React app created for demonstration purposes. The About page provides information about the app.</p>
+      <h1>About <i>Spent</i></h1>
+      <h2>Inspiration</h2>
+      <p>As college students, we have first hand experience of the struggle to allocate our spending money over the course of the semester/school year. Many of our friends and fellow college students have a shared experience where they spend the spending money they make over the summer in the first few weeks back at school and then are left broke for the rest of the semester. Other budgeting apps that allow users to set weekly goals often don't work for college students because every week in college is different. That's where our idea for Spent was born. What if AI told you how much money you are going to spend before you spend it? Gone are the days of budgeting out the small amount of money you have left after you've spent 90% of your savings. It's time to get ahead of your spending and make financial decisions and alterations before the money leaves your wallet.</p>
+      <h2>Our Solution</h2>
+      <p>Spent is a predicted spending app that takes the events in your Google Calendar and uses AI to estimate how much you will spend at each event. It tells you the three different prices each event will cost based on which of three experiences you choose to have (what you order, how you get there, etc.) and allows you to make decisions on your experiences while seeing the impact on your wallet as a whole.</p>
     </div>
   );
 }
